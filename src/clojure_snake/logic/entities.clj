89ab66f84cc6f7f3b09bc824d08a9242ee33847c
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2024-07-25 21:52:56 +0200

    [FEATURE] "grow" method implemented.

--- src/clojure_snake/logic/entities.clj
+++ src/clojure_snake/logic/entities.clj
@@ -10,13 +10,40 @@
 
 (defn new-fruit [state]
   (if (>= (count (:snake state)) (* (:width state (:height state))))
-    ()
+    (); TODO: Manage condition
     (let [new-fruit {:x (Math/round (rand (:width state)))
                      :y (Math/round (rand (:height state)))}]
       (if (some #(= new-fruit %) (rest (:snake state)))
         (recur state)
         (assoc state :food new-fruit))
       )))
+
+(defn make-positions [direction tail]
+  (let [x (:x tail)
+        y (:y tail)]
+     (cond
+       (= (:x direction) 1) [{:x (- x 1) :y y} {:x (+ x 1) :y y} {:x x :y (- y 1)} {:x x :y (+ y 1)}] 
+       (= (:x direction) -1) [{:x (+ x 1) :y y} {:x (- x 1) :y y} {:x x :y (+ y 1)} {:x x :y (- y 1)}]
+       (= (:y direction) 1) [{:x x :y (- y 1)} {:x x :y (+ y 1)} {:x (- x 1) :y y} {:x (+ x 1) :y y}]
+       :else [{:x x :y (+ y 1)} {:x x :y (- y 1)} {:x (+ x 1) :y y} {:x (- x 1) :y y}])))
+
+(defn find-valid-tail [state]
+  (let [posibilities (make-positions (:direction state) (last (:snake state)))]
+    (loop [i 0]
+      (if (< i (count posibilities))
+        (let [position (get posibilities i)]
+          (if (some #(= position %) (:snake state))
+            (recur (inc i))
+            position))
+        nil))))
+
+(defn grow [state]
+  (if (>= (count (:snake state)) (* (:width state (:height state))))
+    (); TODO: Manage condition
+    (let [tail (find-valid-tail state)] 
+      (if (nil? tail)
+        (); TODO: Manage condition
+        (assoc state :snake (conj (:snake state) tail))))))
 
 (defn new-game []
   (new-fruit initial-state))
---
+++ test/clojure_snake/logic/entity_test.clj
@@ -0,0 +1,42 @@
+(ns clojure-snake.logic.entity-test
+  (:require [clojure.test :refer :all]
+            [clojure-snake.logic.entities :refer :all]))
+
+(def test-state-grow-conflict-1
+  {:snake [{:x 5 :y 5} {:x 4 :y 5} {:x 4 :y 4} {:x 5 :y 4}]
+   :direction {:x 1 :y 0}
+   :food {:x 10 :y 10}
+   :width 20
+   :height 20
+   :score 0})
+
+(def test-state-grow-conflict-2
+  {:snake [{:x 5 :y 5} {:x 4 :y 5} {:x 4 :y 4} {:x 5 :y 4}]
+   :direction {:x 0 :y 1}
+   :food {:x 10 :y 10}
+   :width 20
+   :height 20
+   :score 0})
+
+(def test-state-grow-conflict-3
+  {:snake [{:x 5 :y 5} {:x 4 :y 5} {:x 4 :y 4} {:x 5 :y 4}]
+   :direction {:x 0 :y -1}
+   :food {:x 10 :y 10}
+   :width 20
+   :height 20
+   :score 0})
+
+(deftest test-grow-conflict-1
+  (testing "Valide grow conflict"
+    (let [snake (:snake (grow test-state-grow-conflict-1))]
+      (is (= 6 (:x (last snake)))) (is (= 4 (:y (last snake)))))))
+
+(deftest test-grow-conflict-2
+  (testing "Valide grow conflict"
+    (let [snake (:snake (grow test-state-grow-conflict-2))]
+      (is (= 5 (:x (last snake)))) (is (= 3 (:y (last snake)))))))
+
+(deftest test-grow-conflict-3
+  (testing "Valide grow conflict"
+    (let [snake (:snake (grow test-state-grow-conflict-3))]
+      (is (= 5 (:x (last snake)))) (is (= 3 (:y (last snake)))))))
